1ea2004e80b19eaddbf628a3165234e5ba27f1cf
asynchro9us <anmol.hnn@gmail.com>
2025-04-01 03:04:05 +0530

    modified - eslint.config.js (added prettier - partially)

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
 import reactDom from 'eslint-plugin-react-dom'
+import prettier from 'prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -34,12 +35,14 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'react-dom': reactDom,
-      'react': react,
+      react,
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...reactDom.configs.recommended.rules,
       ...react.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
